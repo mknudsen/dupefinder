4de24c5ad3671f013d3b023ee5ed3ba6c164453e
Martin Knudsen <martin.knudsen@linkwerk.com>
2013-03-17 15:46:31 +0100

    fixed url in project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject dupefinder "0.1.0-SNAPSHOT"
   :description "dupefinder find duplicate files below a passed directory"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/mknudsen/dupefinder"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.0"]
